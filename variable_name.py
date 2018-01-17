b3d74271284040d00bfbd42aac846bfcf06eacd0
Bibalskywalker <bibalskywalker@Bibalskywalkers-MacBook-Air.local>
2018-01-17 17:25:43 +0800

    print statement

---
+++ variable_name.py
@@ -0,0 +1,15 @@
+#how to declare a variable and use a variable 
+author = "Killalltheskywalker"
+familiar_language = "HTML,JAVASCIPT,PHP,PYTHON"
+familiar_framework = "Codeigniter,Bootstrap"
+number_phone = 0145002011
+my_gba_point = 0.1
+
+#print standard variable
+print "My name is",author,", My familiar language are",familiar_language,"and my familiar framwework are",familiar_framework ,"."
+
+#check variable type 
+number_phone_type = type(number_phone) #use type() to check the type of variable 
+#print number_phone_type
+
+#print with print formatter
